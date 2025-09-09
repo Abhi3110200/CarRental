cd0cf1e5c304e9cd232dac02ddbb99e2fe443f16
Abhi3110200 <abhijeetdrv@gmail.com>
2025-09-10 04:29:18 +0530

    Auth Routing Done

---
+++ backend/controllers/userController.js
@@ -0,0 +1,60 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const generateToken = (userId) => {
+    return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "2d" });
+}
+
+export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password || password.length < 8) {
+            return res.status(400).json({success: false, message: "All fields are required" });
+        }
+
+        const userExists = await User.findOne({ email });
+        if (userExists) {
+            return res.status(400).json({success: false, message: "User already exists" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({ name, email, password: hashedPassword });
+
+        const token = generateToken(user._id);
+        console.log(token);
+
+        res.status(201).json({success: true, token });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: "Something went wrong" });
+    }
+}
+
+export const loginUser = async(req, res)=>{
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({success: false, message: "All fields are required" });
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({success: false, message: "User not found" });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({success: false, message: "Invalid password" });
+        }
+
+        const token = generateToken(user._id);
+        console.log(token);
+        res.status(200).json({success: true, token });
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false, message: "Something went wrong" });
+    }
+}
--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,23 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
+import connectDB from "./config/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
-
+// /?retryWrites=true&w=majority&appName=Cluster0
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 })
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
 })
