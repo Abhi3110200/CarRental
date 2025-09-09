63aaf8df3f16602f3b155d22de96ad5a5050e54c
Abhi3110200 <abhijeetdrv@gmail.com>
2025-09-10 03:50:30 +0530

    index

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
